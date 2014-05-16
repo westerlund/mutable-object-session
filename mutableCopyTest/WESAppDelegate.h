856429e3f420677def1c10d7542bf994cafee2b6
Simon Westerlund <s@simonwesterlund.se>
2014-05-16 23:05:19 +0200

    moved files around

---
+++ mutableCopyTest/WESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WESAppDelegate.h
+//  mutableCopyTest
+//
+//  Created by Simon Westerlund on 16/05/14.
+//  Copyright (c) 2014 Simon Westerlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
